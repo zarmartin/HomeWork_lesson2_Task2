fb0d4fb697958af14e5187b3a88d348a9d298ade
Martin Zarutski <martinzar5@gmail.com>
2023-06-05 14:32:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static public void Main()
+        {
+            int i = 1, n = -20, summ;
+            
+            do
+            {
+                summ = i * n;
+                Console.WriteLine(Environment.NewLine+ summ);
+                i++;
+            }
+            while (i <= 5);
+          
+        }
+    }   
+
+}
